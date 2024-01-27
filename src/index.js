b2ea1c400dbbc47a35b2d2b38f28f06f29feb570
Elaine Muhombe <emuhombe@gmail.com>
2024-01-27 20:57:37 +0300

    Fix: Bug - import ReactDOM from 'react-dom/client'instead of import ReactDOM from 'react-dom'

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 // index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './store';
-import App from './App';
+import store from './store/Store';
+import App from './App'; // Correct import
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
 );

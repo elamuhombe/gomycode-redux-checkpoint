23e5feff6326b944358558e8a76c114c9e96b632
Elaine Muhombe <emuhombe@gmail.com>
2024-01-27 23:10:58 +0300

    Chore: Update EditTask.js

--- src/components/EditTask.js
+++ src/components/EditTask.js
@@ -2,12 +2,21 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
-const EditTask = ({ task }) => {
+const EditTask = ({ task, onCancel }) => {
   const dispatch = useDispatch();
   const [editedDescription, setEditedDescription] = useState(task.description);
 
-  const handleEditTask = () => {
-    dispatch({ type: 'EDIT_TASK', payload: { id: task.id, description: editedDescription } });
+  const handleSaveChanges = () => {
+    if (editedDescription.trim() !== '') {
+      dispatch({
+        type: 'EDIT_TASK',
+        payload: { id: task.id, description: editedDescription },
+      });
+
+      // You can also add additional logic or callbacks here, if needed
+
+     
+    }
   };
 
   return (
@@ -15,11 +24,15 @@ const EditTask = ({ task }) => {
       <input
         type="text"
         value={editedDescription}
-        onChange={(e) => setEditedDescription(e.target.value)}
+        onChange={(e) => {
+          console.log(e.target.value); // Log the value to the console
+          setEditedDescription(e.target.value);
+        }}
+        placeholder="Enter updated task description"
       />
-      <button onClick={handleEditTask}>Save Changes</button>
+      <button onClick={handleSaveChanges}>Save Changes</button>
     </div>
   );
-};
+      }  
 
 export default EditTask;

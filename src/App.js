bffc05203f19044d9181d35d045285c8b384c7cd
Elaine Muhombe <emuhombe@gmail.com>
2024-01-28 21:04:16 +0300

    Chore: Update App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-
+// Importing React
 import React from 'react';
+
+// Importing components
 import AddTask from './components/AddTask';
+import FilterTask from './components/FilterTask';
 import ListTask from './components/ListTask';
 
+// Functional component for the main application
 const App = () => {
   return (
-    <div>
-      <h1>Todo Application</h1>
+    <div className='task-container'>
+      {/* Application title */}
+      <h1 className='todo-title'>Todo List Application</h1>
+      
+      {/* AddTask component for adding new tasks */}
       <AddTask />
+      
+      {/* FilterTask component for filtering tasks */}
+      <FilterTask />
+      
+      {/* ListTask component for displaying the list of tasks */}
       <ListTask />
     </div>
   );

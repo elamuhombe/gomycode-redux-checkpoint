d4bae2ad708cd8ae44a433b8432035ad6eb1e58e
Elaine Muhombe <emuhombe@gmail.com>
2024-01-28 21:10:30 +0300

    Chore: Update index.js file

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store/Store';
-import App from './App'; // Correct import
+import App from './App'; 
 
+// Using createRoot to render the application into the root element
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Providing the Redux store to the entire application using Provider
   <Provider store={store}>
+    {/* Enabling React Strict Mode */}
     <React.StrictMode>
+      {/* Rendering the main application component */}
       <App />
     </React.StrictMode>
   </Provider>,

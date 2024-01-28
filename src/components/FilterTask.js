caef68a98bc1248cd8029662d00464e5773fcd04
Elaine Muhombe <emuhombe@gmail.com>
2024-01-28 21:06:55 +0300

    Chore: Update FilterTask.js

--- src/components/FilterTask.js
+++ src/components/FilterTask.js
@@ -1,23 +1,32 @@
 // FilterTask.js
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import '../styles.css';
 
+// Action creator function to generate the filter task action
 const filterTask = (filter) => ({
   type: 'FILTER_TASK',
   payload: filter,
 });
 
+// Functional component for task filtering
 const FilterTask = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch(); // Initializing dispatch function for redux actions
 
+  // Function to handle changes in the filter selection
   const handleFilterChange = (event) => {
     const selectedFilter = event.target.value;
+    
+    // Dispatching the filterTask action with the selected filter as payload
     dispatch(filterTask(selectedFilter));
   };
 
   return (
-    <div>
+    <div className = 'filter-task'>
+      {/* Label for the filter dropdown */}
       <label htmlFor="filter">Filter:</label>
+      
+      {/* Dropdown menu for selecting the filter */}
       <select id="filter" onChange={handleFilterChange}>
         <option value="all">All</option>
         <option value="done">Done</option>

38dbe5728c0334c5136c8e176d0810360fbd3e73
Elaine Muhombe <emuhombe@gmail.com>
2024-01-28 21:06:06 +0300

    Chore: Update EditTask.js file

--- src/components/EditTask.js
+++ src/components/EditTask.js
@@ -2,34 +2,39 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
+// Functional component for editing task descriptions
 const EditTask = ({ task, onCancel }) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch(); // Initializing dispatch function for redux actions
   const [editedDescription, setEditedDescription] = useState(task.description);
 
+  // Function to handle saving changes to the task description
   const handleSaveChanges = () => {
-    if (editedDescription.trim() !== '') {
+    if (editedDescription.trim() !== '') { // Check if the edited description is not empty
+      // Dispatching an action to edit the task description in the Redux store
       dispatch({
         type: 'EDIT_TASK',
         payload: { id: task.id, description: editedDescription },
+      });
       
-
-     
-    })
+      // Invoking onCancel function passed as a prop to close the edit mode
+      onCancel();
+    }
   };
 
   return (
     <div>
+      {/* Input field for entering the updated task description */}
       <input
         type="text"
         value={editedDescription}
-        onChange={(e) => {
-          setEditedDescription(e.target.value);
-        }}
+        onChange={(e) => setEditedDescription(e.target.value)}
         placeholder="Enter updated task description"
       />
+      
+      {/* Button to save changes */}
       <button onClick={handleSaveChanges}>Save Changes</button>
     </div>
   );
-      }  
+};
 
 export default EditTask;

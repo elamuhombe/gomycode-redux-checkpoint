06f108cdbc8be89bd70c381b23a83b82627c470f
Elaine Muhombe <emuhombe@gmail.com>
2024-01-28 21:05:06 +0300

    Chore: Update AddTask.js file

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,33 +1,42 @@
 // AddTask.js
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux'; // Importing useDispatch hook from react-redux
+import '../styles.css'; // Importing external styles
 
+// Functional component for adding tasks
 const AddTask = () => {
-  const dispatch = useDispatch();
-  const [description, setDescription] = useState('');
+  const dispatch = useDispatch(); // Initializing dispatch function for redux actions
+  const [description, setDescription] = useState(''); // State for managing task description input
 
+  // Function to handle the addition of a new task
   const handleAddTask = () => {
-    if (description.trim() !== '') {
+    if (description.trim() !== '') { // Check if the task description is not empty
       const newTask = {
-        id: Date.now(),
+        id: Date.now(), // Using the current timestamp as a unique identifier
         description,
         isDone: false,
       };
 
+      // Dispatching an action to add the new task to the Redux store
       dispatch({ type: 'ADD_TASK', payload: newTask });
+      
+      // Resetting the description state after adding the task
       setDescription('');
     }
   };
 
   return (
-    <div>
+    <div className="add-task">
+      {/* Input field for entering task description */}
       <input
         type="text"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
         placeholder="Enter task description"
       />
-      <button onClick={handleAddTask}>Add Task</button>
+      
+      {/* Button to trigger the task addition */}
+      <button onClick={handleAddTask} className='btn-add-task'>Add Task</button>
     </div>
   );
 };

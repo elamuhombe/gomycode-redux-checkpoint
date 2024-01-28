ea5463d3c6410fa2e6368aea3f3f8aa3fc37516e
Elaine Muhombe <emuhombe@gmail.com>
2024-01-28 21:07:51 +0300

    Chore: Update ListTask.js file

--- src/components/ListTask.js
+++ src/components/ListTask.js
@@ -1,10 +1,14 @@
 // ListTask.js
 import React from 'react';
 import { useSelector } from 'react-redux';
-import Task from './Task';
+import Task from './Task'; // Importing the Task component
+import '../styles.css'; // Importing external styles
 
+// Functional component for rendering a list of tasks
 const ListTask = () => {
+  // Using the useSelector hook to access the Redux store state
   const tasks = useSelector((state) => {
+    // Filtering tasks based on the current filter in the Redux store
     if (state.filter === 'done') {
       return state.tasks.filter((task) => task.isDone);
     } else if (state.filter === 'notDone') {
@@ -15,7 +19,8 @@ const ListTask = () => {
   });
 
   return (
-    <div>
+    <div class='task-list'>
+      {/* Mapping through filtered tasks and rendering Task component for each */}
       {tasks.map((task) => (
         <Task key={task.id} task={task} />
       ))}
